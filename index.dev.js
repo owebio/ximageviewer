68959085ffdbf227a1ab242147e6e88a782a33d3
oweb <admin@oweb.io>
2017-03-28 06:09:10 +0900

    fixed file location

--- index.dev.js
+++ index.dev.js
@@ -1,13 +1,13 @@
 var imageCollection = [
-  {title: 'Cuba', url: "../sample/sample_cuba.jpg"},
-  {title: 'Thailand', url: "../sample/sample_thailand.jpg"},
-  {title: 'HongKong', url: "../sample/hongkong.jpg"},
-  {title: 'Ankor Wat', url: "../sample/sample_cambodia.jpg"},
-  {title: 'Seoul', url: "../sample/sample_mask.jpg"},
-  {title: 'Casa Milà', url: "../sample/sample_barcelona.jpg"},
-  {title: 'La Grande Arche', url: "../sample/sample_paris.jpg"},
-  {title: 'Tokyo Metropolitan Government', url: "../sample/sample_tokyo.jpg"},
-  {title: 'Friendship', url: "../sample/sample_webtoon.jpg", copyright : "MRLOBENSTEIN.COM"}
+  {title: 'Cuba', url: "sample/sample_cuba.jpg"},
+  {title: 'Thailand', url: "sample/sample_thailand.jpg"},
+  {title: 'HongKong', url: "sample/hongkong.jpg"},
+  {title: 'Ankor Wat', url: "sample/sample_cambodia.jpg"},
+  {title: 'Seoul', url: "sample/sample_mask.jpg"},
+  {title: 'Casa Milà', url: "sample/sample_barcelona.jpg"},
+  {title: 'La Grande Arche', url: "sample/sample_paris.jpg"},
+  {title: 'Tokyo Metropolitan Government', url: "sample/sample_tokyo.jpg"},
+  {title: 'Friendship', url: "sample/sample_webtoon.jpg", copyright : "MRLOBENSTEIN.COM"}
 ]
 
 var imageItem = imageCollection[Math.floor(Math.random() * imageCollection.length)];
